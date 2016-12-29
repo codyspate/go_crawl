4f23c034e98fef1e9b28051526547ae92e5f0631
codyspate <codyspate@gmail.com>
2016-12-29 12:53:30 -0800

    allow for user to not input url scheme

--- functions.go
+++ functions.go
@@ -13,14 +13,13 @@ func input() (string) {
     reader := bufio.NewReader(os.Stdin)
     fmt.Print("URL: ")
     Homepage, _ := reader.ReadString('\n')
+    if !(strings.contains(Homepage, "://")){
+	    Homepage = "http://" + Homepage
+	  }
     return Homepage
 }
 
 func get_domain_name(rawurl string) string {
-    scheme := "http://"
-    if strings.Contains(rawurl, scheme) == false {
-        log.Panicln("Need url scheme, i.e. http://")
-    }
     u, err := url.Parse(rawurl)
     fmt.Println("Scheme: ", u.Scheme)
     fmt.Println("Host: ", u.Host)
