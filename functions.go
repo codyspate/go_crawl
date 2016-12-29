9210c6f7dfecf38830ce4ebc15520f4455af1090
Dixon Begay <dixonbegay@gmail.com>
2016-12-29 15:22:02 -0700

    Removed commented lines

--- functions.go
+++ functions.go
@@ -9,35 +9,6 @@ import (
     "strings"
 )
 
-// func input() (string) {
-//     reader := bufio.NewReader(os.Stdin)
-//     fmt.Print("URL: ")
-//     Homepage, _ := reader.ReadString('\n')
-//     if !(strings.Contains(Homepage, "://")){
-// 	    Homepage = "http://" + Homepage
-//     }
-//     return Homepage
-// }
-//
-// func get_domain_name(raw_url string) string {
-//     var new_url string
-//     scheme := "http://"
-//     if !strings.Contains(raw_url, scheme) {
-//         s := []string{scheme, raw_url}
-//         new_url = strings.Join(s, "")
-//         // log.Panicln("Need url scheme, i.e. http://")
-//     } else {
-//         new_url = raw_url
-//     }
-//     u, err := url.Parse(new_url)
-//     fmt.Println("Scheme: ", u.Scheme)
-//     fmt.Println("Host: ", u.Host)
-// 	if err != nil {
-// 		log.Fatalln(err)
-// 	}
-//     return u.Host
-// }
-
 func input() (string) {
     scan := bufio.NewScanner(os.Stdin)
     fmt.Print("Input URL: ")
