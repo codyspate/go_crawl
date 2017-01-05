7d0a07a3e0bf77da7047ee10e0612955a1170fc3
dixon13 <dixonbegay@gmail.com>
2017-01-05 13:36:56 -0700

    Added function create_data_files, delete_file_contents

--- functions.go
+++ functions.go
@@ -11,6 +11,13 @@ import (
 	//"io/ioutil"
 )
 
+func checkError(err error) {
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(0)
+	}
+}
+
 func check(e error) {
 	if e != nil {
 		fmt.Println("*********************ERROR*******************")
@@ -41,6 +48,36 @@ func get_domain_name(rawurl string) string {
 	return u.Host
 }
 
+// exists returns whether the given file or directory exists or not
+func exists(path string) bool {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true
+	}
+	if os.IsNotExist(err) {
+		return false
+	}
+	fmt.Println(err)
+	return true
+}
+
+func write_file(path string, text string) {
+	// open file using READ & WRITE permission
+	var file, err = os.OpenFile(path, os.O_RDWR, 0644)
+	checkError(err)
+	defer file.Close()
+
+	// write some text to file
+	_, err = file.WriteString(text)
+	checkError(err)
+	_, err = file.WriteString(text)
+	checkError(err)
+
+	// save changes
+	err = file.Sync()
+	checkError(err)
+}
+
 func create_dirs() {
 	var path bytes.Buffer
 	path.WriteString("projects" + string(filepath.Separator) + project_name)
@@ -57,5 +94,44 @@ func create_dirs() {
 		f, _ := os.Create(a[i])
 		// check(e)
 		f.Close()
+	}
+}
+
+// Delete the contents of a file
+func delete_file_contents(path string) {
+	var file, err = os.OpenFile(path, os.O_RDWR, 0644)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(0)
+	}
+	defer file.Close()
+}
+
+// Create queue and crawled files (if not created)
+func create_data_files(project_name string, base_url string) {
+	queue := filepath.Join(".\\projects", project_name, "queue.txt")
+	crawled := filepath.Join(".\\projects", project_name, "crawled.txt")
+	summary := filepath.Join(".\\projects", project_name, "summary.txt")
+	errors := filepath.Join(".\\projects", project_name, "errors.txt")
+	fmt.Println(queue)
+	fmt.Println(crawled)
+	fmt.Println(summary)
+	fmt.Println(errors)
+	delete_file_contents(queue)
+	delete_file_contents(crawled)
+	delete_file_contents(summary)
+	delete_file_contents(errors)
+	if exists(queue) {
+		write_file(queue, base_url)
+	}
+	if exists(crawled) {
+		write_file(crawled, "")
+	}
+	if exists(summary) {
+		t := "PDF count:     \nHTML/HTM count:     \nMedia files:     \nOther:     \nErrors:     \n\nTotal size:     "
+		write_file(summary, t)
+	}
+	if exists(errors) {
+		write_file(errors, "")
 	}
 }
