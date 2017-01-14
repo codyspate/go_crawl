d009470ef51cb9e9328efe243d880e34abe1876c
dixon13 <dixonbegay@gmail.com>
2017-01-13 17:03:54 -0700

    WIP: started adding parser for html pages

--- tests/main.go
+++ tests/main.go
@@ -9,7 +9,7 @@ import (
 
 func main() {
 	urlPtr := flag.String("url", "http://www.google.com/", "a url <string>")
-	threadPtr := flag.Int("threads", 10, "number of threads <int>")
+	threadPtr := flag.Int("threads", 50, "number of threads <int>")
 	flag.Parse()
 	// Below is from Python version
 	// 71stsog | http://71stsog.com/ | 71stsog.com
